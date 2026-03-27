74412a13f5137ec79480628e3ce7115111710e9c
Behcacrabat <benjatabarcano@gmail.com>
2026-03-27 16:50:37 -0300

    Add files via upload

---
+++ 3_TabarcacheB_V/TP 3/TP 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola usuario");
+            Console.Write("¿Cual es tu año de nacimiento?: ");
+            int año = Convert.ToInt32(Console.ReadLine());
+            int edad = 2026 - año;
+
+            Console.WriteLine("Si naciste en el año " + año + " y tu edad al comenzar el año es de " + edad + ".");
+         
+        }
+    }
+}
