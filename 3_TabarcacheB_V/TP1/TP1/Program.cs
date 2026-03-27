45da55aa5a6045aa539ebff89cfa131a4a02cc43
Behcacrabat <benjatabarcano@gmail.com>
2026-03-27 16:06:28 -0300

    Add files via upload

---
+++ 3_TabarcacheB_V/TP1/TP1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string pelicula;
+
+            Console.WriteLine("Hola Pibe");
+            Console.Write("¿Como es tu nombre?: ");
+            nombre = Console.ReadLine();
+            Console.Write("¿Cual es tu pelicula favorita?: ");
+            pelicula = Console.ReadLine();
+
+            Console.WriteLine("Hola " + nombre + "." + "Tu pelicula preferida " + pelicula + " es una porqueria.");
+
+
+
+        }
+    }
+}
