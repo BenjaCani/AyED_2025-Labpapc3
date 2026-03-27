7ea23b348454404cb72192bf684acb14de73519d
Behcacrabat <benjatabarcano@gmail.com>
2026-03-27 16:45:12 -0300

    Add files via upload

---
+++ 3_TabarcacheB_V/TP 2/TP 2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola usuario");
+            Console.Write("¿Cual es tu nombre?: ");
+            string nombre = Console.ReadLine();
+            Console.Write("¿Cual es tu VideoJuego favorito?: ");
+            string juego = Console.ReadLine();
+            Console.Write("¿En que consola Jugas?: ");
+            string consola = Console.ReadLine();
+
+            Console.WriteLine("Hola " + nombre + "." + "Tu juego favorito es " + juego + " y se juega en " + consola + ".");
+
+
+
+
+        }
+    }
+}
