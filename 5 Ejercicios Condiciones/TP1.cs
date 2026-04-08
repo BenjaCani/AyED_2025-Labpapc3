bd784f2f724ded78c2a0d0c54d630a6a8c1719b7
Behcacrabat <benjatabarcano@gmail.com>
2026-04-08 15:52:49 -0300

    Add files via upload

---
+++ 5 Ejercicios Condiciones/TP1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PesoNave
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el peso de su nave: ");
+            float peso = Convert.ToInt32(Console.ReadLine());
+            double descarga = peso - 5000;
+
+            if (peso <= 5000)
+            {
+                Console.WriteLine("Despegue autorizado. Buen viaje, Mando");
+            }
+            else
+            {
+                Console.WriteLine("Exceso de peso. Debes descargar " + descarga + "kg para despegar");
+            }
+                
+        }
+    }
+}
