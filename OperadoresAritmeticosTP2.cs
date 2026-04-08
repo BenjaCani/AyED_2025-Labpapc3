b8acfaa673344255100527d482f8db114e0c20c2
Behcacrabat <benjatabarcano@gmail.com>
2026-04-08 14:30:42 -0300

    Add files via upload

---
+++ OperadoresAritmeticosTP2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperadoresAritmeticosTP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¿Cuantos minutos estuvo estacionado? (especifique en numeros)");
+            int minutos = Convert.ToInt32(Console.ReadLine());
+            int hora = minutos / 60;
+            int resto = minutos % 60; 
+
+            Console.WriteLine("Estacionado: " + hora + " horas " + resto + " minutos");
+
+            int CostoxHora = 6000 * hora;
+            int CostoxMinuto = 150 * resto;
+            int CostoTotal = CostoxHora + CostoxMinuto;
+            Double CostoFinal = CostoTotal / 1.21;
+
+            Console.WriteLine("Monto Final a pagar: " + CostoFinal + "$");
+
+
+
+        
+        }
+    }
+}
