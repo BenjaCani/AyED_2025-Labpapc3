4226e5f7b0c67aa353daa4e3663bdf29f57c766f
Behcacrabat <benjatabarcano@gmail.com>
2026-04-10 16:25:21 -0300

    Add files via upload

---
+++ Ejercicios Condicionales - Primera parte/TP10.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {          
+            Console.WriteLine("Ingresa el Presupuesto disponible: ");
+            float Press = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa el costo de los materiales: ");
+            float CostoMateriales = Convert.ToInt32(Console.ReadLine());
+
+
+            if (CostoMateriales > Press)
+            {
+                float faltante = CostoMateriales - Press;
+                Console.WriteLine("El costo es mayor al presupuesto." + "Faltan " + faltante+ " $." );
+            }
+            else
+            {
+                Console.WriteLine("Si alcanza.");
+            }
+
+        }
+        
+    }
+}
