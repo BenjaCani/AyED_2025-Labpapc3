4248347e2490be47401f7a3bcee2132f7db987bd
Behcacrabat <benjatabarcano@gmail.com>
2026-04-10 13:58:53 -0300

    Add files via upload

---
+++ 5 Ejercicios Condiciones/TP5.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("UN TES DE PERSONALIDAD BASADO EN ACCIONES");
+        Console.WriteLine("- Encuentras un cristal Kyber perdido -");
+        Console.WriteLine("- ¿Que haces? -");
+
+        Console.WriteLine("Opción 1: Lo devuelves al templo.");
+        Console.WriteLine("Opción 2: Lo vendes por créditos.");
+        Console.WriteLine("Opcion 3: Lo usas para construir un arma roja.");
+
+        Console.WriteLine("(Escribe el numero de la opcion que eliges)");
+        int respuesta = Convert.ToInt32(Console.ReadLine());
+
+        if (respuesta == 1)
+        {
+            Console.WriteLine("El camino del Jedi sigue.");
+        }
+        else if (respuesta == 2)
+        {
+            Console.WriteLine("Un cazarrecompensas eres.");
+        }
+        else
+        {
+            Console.WriteLine("El Lado Oscuro se apodera de ti.");
+        }
+
+    }
+}
