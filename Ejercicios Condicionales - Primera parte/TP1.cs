d6cf1e2bd97a3c2c240d1a657a07849b395e73a8
Behcacrabat <benjatabarcano@gmail.com>
2026-04-10 14:40:22 -0300

    Add files via upload

---
+++ Ejercicios Condicionales - Primera parte/TP1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Escribe tu Nombre : ");
+        string Nombre = Console.ReadLine();
+        Console.WriteLine("Indique la velocidad de su vehiculo: ");
+        int edad = Convert.ToInt32(Console.ReadLine());
+        if (edad >= 18)
+        {
+            Console.WriteLine("Bienvenido al club " + Nombre + ".");
+        }
+        else
+        {
+            Console.WriteLine("Lo siento, eres muy polluelo.");
+        }
+
+    }
+}
---
+++ Ejercicios Condicionales - Primera parte/TP2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Escribe tu Nombre : ");
+        string Nombre = Console.ReadLine();
+        Console.WriteLine("Indique su edad: ");
+        int edad = Convert.ToInt32(Console.ReadLine());
+        if (edad >= 18)
+        {
+            Console.WriteLine("Bienvenido al club " + Nombre + ".");
+        }
+        else
+        {
+            Console.WriteLine("Lo siento, eres muy polluelo.");
+        }
+
+    }
+}
