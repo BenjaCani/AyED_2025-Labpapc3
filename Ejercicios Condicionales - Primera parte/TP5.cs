87e6edb43da1f2c6ce9dd230d5f143492c03b6ed
Behcacrabat <benjatabarcano@gmail.com>
2026-04-10 15:24:26 -0300

    Add files via upload

---
+++ Ejercicios Condicionales - Primera parte/TP5.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Ingrese el monto de la comprar:");
+        int monto = Convert.ToInt32(Console.ReadLine());
+
+        if (monto >= 5000)
+        {
+            double descuento = monto * 0.15;
+            double TotalFinal = Total - descuento;
+            Console.WriteLine("Total con descuento: " + TotalFinal + " creditos");
+        }
+        else
+        {
+            Console.WriteLine("Total original: " + Total + " creditos");
+        }
+    }
+}
