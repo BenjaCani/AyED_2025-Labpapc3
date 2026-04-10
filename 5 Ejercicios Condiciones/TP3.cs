6ae04eb7a038e2423741a7712bb4620fbc43be58
Behcacrabat <benjatabarcano@gmail.com>
2026-04-10 13:55:34 -0300

    Add files via upload

---
+++ 5 Ejercicios Condiciones/TP3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese la distancia (años luz): ");
+        double distancia = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Ingresa la cantidad combustible actual: ");
+        double combustible = Convert.ToDouble(Console.ReadLine());
+        double combustibleNecesario = distancia * 12;
+
+        if (combustible >= combustibleNecesario)
+        {
+            Console.WriteLine("Cálculos precisos. Saltando al hiperespacio");
+        }
+        else
+        {
+            double faltante = combustibleNecesario - combustible;
+            Console.WriteLine("Peligro: Combustible insuficiente. Faltan " + faltante + " litros");
+        }
+    }
+}
