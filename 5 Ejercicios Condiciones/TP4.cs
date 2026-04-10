1dee1df74a522c64a36fce4f44de615f7e9bd31f
Behcacrabat <benjatabarcano@gmail.com>
2026-04-10 13:57:29 -0300

    Add files via upload

---
+++ 5 Ejercicios Condiciones/TP4.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Ingrese la cantidad de droides a comprar:");
+        int cantidad = Convert.ToInt32(Console.ReadLine());
+        double Total = cantidad * 2000;
+
+        if (cantidad <= 2)
+        {
+            Console.WriteLine("Total a pagar: " + Total + " creditos");
+        }
+        else
+        {
+            double descuento = Total * 0.20;
+            double TotalFinal = Total - descuento;
+            Console.WriteLine("Total original: " + Total + " creditos");
+            Console.WriteLine("Ahorrado: " + descuento + " creditos");
+            Console.WriteLine("El Total Final: " + TotalFinal + " creditos");
+        }
+    }
+}
