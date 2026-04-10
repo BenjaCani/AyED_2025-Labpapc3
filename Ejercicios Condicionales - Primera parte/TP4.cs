56f43a42944a5b1ad05cd1266a6ee5fa558acc77
Behcacrabat <benjatabarcano@gmail.com>
2026-04-10 14:34:47 -0300

    Add files via upload

---
+++ Ejercicios Condicionales - Primera parte/TP4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("¿ Que nota sacaste en el examen ? : ");
+        double nota = Convert.ToDouble(Console.ReadLine());
+
+        if (nota >= 7)
+        {
+            Console.WriteLine("Estas Promocionado!!");
+        }
+        else if (nota >= 4 && nota <= 6)
+        {
+            Console.WriteLine("A Finales");
+        }
+        else
+        {
+            Console.WriteLine("A Recuperatorio");
+
+        }
+    }
+}
