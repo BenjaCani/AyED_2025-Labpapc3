c160a6390bf95a05053bb34ef0db2959b5a1b6c5
Behcacrabat <benjatabarcano@gmail.com>
2026-04-15 16:17:20 -0300

    Add files via upload

---
+++ Condicionales - Segunda Parte/TP10.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionales___Segunda_Parte
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cantidad de facturas producidas al inicio del día: ");
+            int FacturaAlInicioDelDIa = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Cuántas docenas completas (12 unidades) se vendieron: ");
+            int CantidadVendidas = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Precio por docena: ");
+            double PrecioXdocena= Convert.ToDouble(Console.ReadLine());
+
+            int Sobraron = FacturaAlInicioDelDIa - CantidadVendidas;
+            double PlataGanada = CantidadVendidas * PrecioXdocena;
+
+            
+            Console.WriteLine("Dinero Recaudado:" + PlataGanada);
+
+            if (Sobraron > 6)
+            {
+                Console.WriteLine("Sobraron:" + Sobraron + " facturas.");
+                Console.WriteLine("Alerta: Exceso de producción");      
+            }
+
+            else if (Sobraron <= 6)
+            {
+                Console.WriteLine("Producción eficiente");
+            }
+        }
+    }
+}
