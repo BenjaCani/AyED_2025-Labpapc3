55975e7aa9de072c5b5c5a75651b49bb595b2ad2
Behcacrabat <benjatabarcano@gmail.com>
2026-04-15 16:24:23 -0300

    Add files via upload

---
+++ Condicionales - Segunda Parte/TP2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionales___Segunda_Parte
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peso: ");
+            double Peso = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Altura: ");
+            double Altura = Convert.ToDouble(Console.ReadLine());
+
+            double IMC = Peso / (Altura * Altura);
+
+            if (IMC > 25)
+            {
+                Console.WriteLine("Sobrepeso");      
+            }
+
+            else if (IMC < 25)
+            {
+                Console.WriteLine("Rango normal");
+            }
+        }
+    }
+}
