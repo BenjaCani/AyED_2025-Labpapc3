6509cf5d9072d12db53b7fcd86e43338c13d7a0b
Behcacrabat <benjatabarcano@gmail.com>
2026-04-15 15:21:52 -0300

    Add files via upload

---
+++ Condicionales - Segunda Parte/TP5.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Condicionales___Segunda_Parte
+{
+    class Program
+    {
+ 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escibe una cantidad de Segundos: ");
+            int segundos = Convert.ToInt32(Console.ReadLine());
+
+            if (segundos >= 60)
+            {
+                int minutos = segundos / 60;
+                int restoSegundos = segundos % 60;
+
+                Console.WriteLine("Convertidor de Segundos: " + minutos + " minuto " + restoSegundos + " segundos .") ;
+            }
+            else if (segundos < 60)
+            {
+                Console.WriteLine(segundos + " segundos" );
+            }
+        }
+    }
+}
