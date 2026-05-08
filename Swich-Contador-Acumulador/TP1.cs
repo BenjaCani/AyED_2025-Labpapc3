bfed1f7eab99d4a053c12f691c60e225e60c9aab
Behcacrabat <benjatabarcano@gmail.com>
2026-05-08 14:07:53 -0300

    Add files via upload

---
+++ Swich-Contador-Acumulador/TP1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-Lista de Estaciones del año-");
+            Console.WriteLine("1. Primavera");
+            Console.WriteLine("2. Verano");
+            Console.WriteLine("3. Otoño");
+            Console.WriteLine("4. Invierno");
+            Console.Write("Escribe tu opcion favorita: ");
+            int opciones = int.Parse(Console.ReadLine());
+
+            switch (opciones)
+            {
+                case 1:
+                    Console.WriteLine("Primavera!");
+                    break;
+                case 2:
+                    Console.WriteLine("Verano!");
+                    break;
+                case 3:
+                    Console.WriteLine("Otoño!");
+                    break;
+                case 4:
+                    Console.WriteLine("Invierno!");
+                    break;
+
+            }
+
+
+
+        }
+    }
+}
