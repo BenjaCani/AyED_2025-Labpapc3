7d3abc76272bdcc31272959df37f3ae78483f6b9
Behcacrabat <benjatabarcano@gmail.com>
2026-05-08 15:31:39 -0300

    Add files via upload

---
+++ Swich-Contador-Acumulador/TP6.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-Lista de Comidas-");
+            Console.WriteLine("(S) Sandwich");
+            Console.WriteLine("(P) Pizza");
+            Console.WriteLine("(H) Hamburguesa");
+            Console.Write("Que menu quiere elegir (S-P-H): ");
+            string menu = Console.ReadLine();
+
+            switch (menu)
+            {
+                case "S":
+                case "s":
+                    Console.WriteLine("Haz elegido Sandwich");
+                    break;
+                case "P":
+                case "p":
+                    Console.WriteLine("Haz elegido Pizza");
+                    break;
+                case "H":
+                case "h":
+                    Console.WriteLine("Haz elegido Hamburguesa");
+                    break;
+
+            }
+
+
+        }
+    }
+}
