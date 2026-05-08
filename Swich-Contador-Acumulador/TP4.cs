fde9767f3eb0b25184f68c7a268ceb13ddb9aadc
Behcacrabat <benjatabarcano@gmail.com>
2026-05-08 15:20:28 -0300

    Add files via upload

---
+++ Swich-Contador-Acumulador/TP4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.Write("Ingresa el Numero N°" + i + ":");
+                double numero = double.Parse(Console.ReadLine());
+
+                if (numero > 100)
+                {
+                    contador++;
+                }
+            }
+
+            Console.Write("Numeros mayor a 100: " + contador);
+        }
+    }
+}
