e2c1de032bb6f71f01c6df31a718afc7a8425b17
Behcacrabat <benjatabarcano@gmail.com>
2026-05-08 15:41:32 -0300

    Add files via upload

---
+++ Swich-Contador-Acumulador/TP9.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Programa
+{
+    static void Main()
+    {
+        double total = 0;
+        int contadorMayores500 = 0;
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.Write("Ingrese el gasto " + i + ":");
+            double gasto = double.Parse(Console.ReadLine());
+            total = total + gasto;
+            Console.WriteLine("Acumulado: " + total);
+
+            if (gasto > 500)
+            {
+                contadorMayores500 = contadorMayores500 + 1;
+            }
+        }
+
+        Console.WriteLine("Gasto total: " + total + "$");
+        Console.WriteLine("Cantidad de gastos mayores a $500: " + contadorMayores500);
+    }
+}
