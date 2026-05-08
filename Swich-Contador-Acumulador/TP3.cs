1d48882352bf14b43d90994da0c0f150465535fa
Behcacrabat <benjatabarcano@gmail.com>
2026-05-08 14:35:22 -0300

    Add files via upload

---
+++ Swich-Contador-Acumulador/TP3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double ahorrado = 0;
+
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.Write("Guardar: ");
+                double ahorro = double.Parse(Console.ReadLine());
+                ahorrado = ahorrado + ahorro;
+                Console.WriteLine("Ahorrados: "+ahorrado);
+
+
+
+            }
+
+        }
+    }
+
+}
+
