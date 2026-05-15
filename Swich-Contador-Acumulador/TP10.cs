d8cd195b5be39abd36054d43ad19db66075778c1
Behcacrabat <benjatabarcano@gmail.com>
2026-05-15 14:50:19 -0300

    Add files via upload

---
+++ Swich-Contador-Acumulador/TP10.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa un número entero positivo: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int divisores = 0;
+        for (int i = 1; i <= numero; i++)
+        {
+            if (numero % i == 0)
+            {
+                divisores++;
+            }
+        }
+
+        if (divisores == 2)
+        {
+            Console.WriteLine("Es primo");
+        }
+        if (divisores != 2)
+        {
+            Console.WriteLine("No es primo");
+            Console.WriteLine("Primer divisor encontrado (aparte del 0): " + divisores);
+        }
+    }
+}
