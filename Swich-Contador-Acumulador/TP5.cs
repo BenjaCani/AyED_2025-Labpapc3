51177b47ee2547b34a770c26118208f4197274cd
Behcacrabat <benjatabarcano@gmail.com>
2026-05-15 14:44:13 -0300

    Add files via upload

---
+++ Swich-Contador-Acumulador/TP5.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingresa un número: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        int divisores = 0;
+        for (int i = 1; i <= numero; i++)
+        {
+            if (numero % i == 0)
+            {
+                divisores++;
+            }
+        }
+
+        if (divisores == 2)
+        {
+            Console.WriteLine("Es primo");
+        }
+        if (divisores != 2)
+        {
+            Console.WriteLine("No es primo");
+        }
+    }
+}
