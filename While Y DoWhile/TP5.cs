eeb70eb4d2878fadcb558e1ed7079158afddb7af
Behcacrabat <benjatabarcano@gmail.com>
2026-05-20 16:00:29 -0300

    Add files via upload

---
+++ While Y DoWhile/TP5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string claveUsuario;
+            string claveReal = "1234";
+
+            do
+            {
+                
+                Console.Write("Ingrese la Clave: ");
+                claveUsuario = Console.ReadLine();
+               
+
+
+            } while (claveReal != claveUsuario);
+            {
+
+                Console.Write("Acceso Permitido");
+
+
+            }
+
+        }
+    }
+}
