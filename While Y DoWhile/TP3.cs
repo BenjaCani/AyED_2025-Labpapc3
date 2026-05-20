461c135db6ecda1f5d2f30f193262a37fdea2c9a
Behcacrabat <benjatabarcano@gmail.com>
2026-05-20 15:38:24 -0300

    Add files via upload

---
+++ While Y DoWhile/TP3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numeros;
+            int alcancia = 0;
+            do
+            {
+                Console.Write("Guardar en alcancia: ");
+                numeros = int.Parse(Console.ReadLine());
+                alcancia = alcancia + numeros;
+            } while (numeros > 0);
+            {
+                Console.Write("Total Acumulado: "+ alcancia);
+
+
+            }
+        }
+    }
+}
