2fc34b4bb9ec853485620afc7098d1de27cb36a8
Behcacrabat <benjatabarcano@gmail.com>
2026-05-20 15:18:02 -0300

    Add files via upload

---
+++ While Y DoWhile/TP2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Write("Escribe la nota de tu examen: ");
+            int nota = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.Write("Escribe la nota de tu examen: ");
+                nota = int.Parse(Console.ReadLine());
+
+
+
+            } while (nota > 10);
+            {
+
+                Console.Write("Error");
+
+
+            }
+            Console.Write("Escribe la nota de tu examen: ");
+        }
+    }
+}
