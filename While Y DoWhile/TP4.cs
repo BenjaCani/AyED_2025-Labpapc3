22307f1cebbc320138bd460b98c7bff18a0669b4
Behcacrabat <benjatabarcano@gmail.com>
2026-05-20 15:48:49 -0300

    Add files via upload

---
+++ While Y DoWhile/TP4.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Tabla de Multiplicar del N°");
+            int numero = int.Parse(Console.ReadLine());
+            int contador = 0;
+
+            while (contador <= 12)
+            {
+                Console.WriteLine(numero + " x " + contador);
+                int calculo = numero * contador;
+                contador++;
+            }
+
+        }
+    }
+}
