d79ff40787766f374f1525f65684db472b29b8b0
Behcacrabat <benjatabarcano@gmail.com>
2026-05-22 15:56:34 -0300

    Add files via upload

---
+++ While Y DoWhile/TP10.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A;
+            int B;
+            int resultado = 0;
+            int contador = 0;
+
+            Console.Write("Ingrese el primer numero: ");
+            A = int.Parse(Console.ReadLine());
+
+            Console.Write("Ingrese el segundo numero: ");
+            B = int.Parse(Console.ReadLine());
+
+            while (contador < B)
+            {
+                resultado = resultado + A;
+                contador = contador + 1;
+            }
+
+            Console.WriteLine("El resultado de la multiplicacion es: " + resultado);
+        }
+    }
+}
