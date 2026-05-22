a85cfb110d470b7c9a3a5886ca00b6936cc73a0d
Behcacrabat <benjatabarcano@gmail.com>
2026-05-22 14:30:11 -0300

    Add files via upload

---
+++ While Y DoWhile/TP6.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("ingrese un numero: ");
+            int numero = int.Parse(Console.ReadLine());
+            int contador = 1;
+
+            while (contador <= numero)
+            {
+                if (contador % 2 == 0)
+                {
+                    Console.WriteLine("Numeros Pa: " + contador);
+
+                }
+                contador++;
+            }
+        }
+    }
+}
