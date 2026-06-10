0383a1b9564bc87809b4751e76eb715a01e8483f
Behcacrabat <benjatabarcano@gmail.com>
2026-06-10 16:17:53 -0300

    Add files via upload

---
+++ While y DoWhile 3/TP1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int presentes = 0;
+            int ausentes = 0;
+            int alumnos_cant = 1;
+            string respuesta = "si"+ "Si";
+
+            do
+            {
+                Console.WriteLine("Nombre del Alumno: ");
+                string Nombre_Alum = Console.ReadLine();
+                
+
+                Console.WriteLine("Estado: ");
+                string Estado = Console.ReadLine();
+
+                if (Estado == "P" || Estado == "p")
+                {
+                    presentes++;
+                    
+                    Console.WriteLine("Quiere agregar otro alumno? (si o no)");
+                    respuesta = Console.ReadLine();
+                    if (respuesta == "si" || Estado == "Si")
+                    {
+                        alumnos_cant++;
+
+                    }
+
+                }
+                else if (Estado == "A" || Estado == "a")
+                {
+                    ausentes++;
+                    Console.WriteLine("Quiere agregar otro alumno? (si o no)");
+                    respuesta = Console.ReadLine();
+                    if (respuesta == "si" || Estado == "Si")
+                    {
+                        alumnos_cant++;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("ERROR");
+                }
+
+
+            } while (respuesta == "si" || respuesta == "Si" );
+
+            Console.WriteLine("Cantidad de alumnos: " + alumnos_cant);
+            Console.WriteLine("Cantidad de presentes: "+presentes);
+            Console.WriteLine("Cantidad de ausentes: " +ausentes);
+            Console.WriteLine("Porcentaje de Presentismo sobre el total de alumnos: " + 100*(presentes / alumnos_cant)+"%" );
+
+
+
+
+        }
+    }
+}
---
+++ While y DoWhile 3/TP3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int personas_cant=0;
+            int suma_Edad = 0;
+            string respuesta = "si" + "Si";
+            int mayores = 0;
+            int menores = 0;
+
+            do
+            {
+                Console.WriteLine("Ingrese la edad: ");
+                int edad = int.Parse(Console.ReadLine());
+                personas_cant++;
+                suma_Edad = suma_Edad + edad;
+                if (edad >= 18)
+                {
+                    mayores++;
+                }
+                else
+                {
+                    menores++;
+                }
+
+                Console.WriteLine("Desea seguir ingresando edades? (si/no) ");
+                respuesta = Console.ReadLine();
+
+            } while (respuesta == "si" || respuesta == "Si" );
+
+            float promedio = suma_Edad / personas_cant;
+            Console.WriteLine("El promedio de edad de todas las personas registradas: "+promedio);
+            Console.WriteLine("Mayores de edad: "+mayores);
+            Console.WriteLine("Menores de edad: "+menores);
+
+
+
+        }
+    }
+}
