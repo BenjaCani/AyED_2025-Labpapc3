44773445f3edd2c0f3bdac3a0ab3167558851e74
Behcacrabat <benjatabarcano@gmail.com>
2026-08-28 15:16:56 -0300

    Add files via upload

---
+++ Funciones/TP 3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumaa = 0;
+            Console.WriteLine("Cuenta regresiva:");
+            int Numero = int.Parse(Console.ReadLine());
+            ContarRegresivo(Numero,sumaa);
+            
+            
+        }
+
+        static void ContarRegresivo(int numero, int suma)
+        {
+            
+            for (int i = 1; i <= numero; i++)
+            {
+                suma += i;
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Suma de los Numeros = "+suma);
+        }
+
+    }
+}
