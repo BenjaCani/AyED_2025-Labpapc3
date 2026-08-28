f45d634d988a3340efa7c6437c23991c1b5dda09
Behcacrabat <benjatabarcano@gmail.com>
2026-08-28 15:25:04 -0300

    Add files via upload

---
+++ Funciones/TP 5.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int La_Base = 2;
+            Console.Write($"Potencia de {La_Base}:");
+            int Exponente = int.Parse(Console.ReadLine());
+            ContarRegresivo(La_Base, Exponente);
+            
+            
+        }
+
+        static void ContarRegresivo(int La_Basee, int Exponentee)
+        {
+            
+            for (int i = 1; i <= La_Basee; i++)
+            {
+                Exponentee *= i;
+                
+            }
+            Console.WriteLine("Suma de los Numeros = "+ Exponentee);
+        }
+
+    }
+}
