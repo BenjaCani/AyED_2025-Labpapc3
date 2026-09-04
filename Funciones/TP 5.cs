07087825568cc9ef24fc73c38d341622f76782ee
Behcacrabat <benjatabarcano@gmail.com>
2026-09-04 14:47:08 -0300

    Update TP 5.cs

--- Funciones/TP 5.cs
+++ Funciones/TP 5.cs
@@ -31,3 +31,80 @@ namespace ConsoleApplication4
 
     }
 }
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Desarrollar un programa que permita ingresar una cantidad determinada de números enteros. Al
+            finalizar la carga, mostrar el mayor, el menor, el promedio de los valores ingresados y la
+            cantidad de números pares e impares.*/
+
+            int cantidad;
+            Console.Write("Hola! Cuantos Numeros deseas ingresar? ");
+            cantidad =  int.Parse(Console.ReadLine());
+
+            while ()
+            {
+
+            }
+          
+        }
+
+        static void Mayor(int a, int b)
+        {
+            if (a > b)
+            {
+                return ;
+            }
+            else
+            {
+                return ;
+            }
+
+        }
+        static void Menor(int a, int b)
+        {
+            
+
+            if (a < b)
+            {
+                return ;
+            }
+            else
+            {
+                return ;
+            }
+
+        }
+        static void Promedio(int Cantidad, int parametro, int Promedio)
+        {
+
+            Promedio = parametro / Cantidad;
+            Console.WriteLine("Promedio Total: ");
+
+        }
+        static void Par_Impar(int numero)
+        {
+           if (numero % 2 == 0)
+           {
+               Console.WriteLine("Si es par.");
+           }
+           else
+           {
+               Console.WriteLine("Es impar");
+           }
+
+        }
+
+    }
+}
